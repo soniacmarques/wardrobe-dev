24cec29aeda6e146787dbd27c0bb38e93f3ef73b
soniacmarques <soniacmarques2@gmail.com>
2026-05-15 18:37:59 +0100

    fix api errors

--- apps/api/main.py
+++ apps/api/main.py
@@ -9,17 +9,24 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 class ItemBase(BaseModel):
+    """Base model for item"""
+
     title: str
-    content: str
+    description: str
     user_id: int
 
+
 class UserBase(BaseModel):
+    """Base model for user"""
+
     username: str
 
 
 def get_db():
-    db=SessionLocal()
+    """Get database session"""
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -30,37 +37,48 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(item:ItemBase, db: db_dependency):
+async def create_item(item: ItemBase, db: db_dependency):
+    """Create new item"""
     db_item = models.Item(**item.model_dump())
     db.add(db_item)
     db.commit()
+    db.refresh(db_item)
+    return db_item
+
 
 @app.get("/items/{item_id}", status_code=status.HTTP_200_OK)
 async def read_item(item_id: int, db: db_dependency):
-    """ Read item by id"""
+    """Read item by id"""
     item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
 
 
-
-@app.delete(f"/items/{item_id}", status_code=status.HTTP_200_OK)
-async def delete_item(item:ItemBase, db: db_dependency):
+@app.delete("/items/{item_id}", status_code=status.HTTP_200_OK)
+async def delete_item(item_id: int, db: db_dependency):
+    """Delete item by id"""
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
-    if item is None:
+    if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     db.delete(db_item)
     db.commit()
+    return {"detail": "Item deleted"}
+
 
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
-async def create_user(user:UserBase, db: db_dependency):
+async def create_user(user: UserBase, db: db_dependency):
+    """Create new user"""
     db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
+    db.refresh(db_user)
+    return db_user
+
 
 @app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
 async def read_user(user_id: int, db: db_dependency):
+    """Read user by id"""
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
--- apps/api/models.py
+++ apps/api/models.py
@@ -3,15 +3,20 @@ from sqlalchemy import Boolean, Column, Integer, String
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """Base model for user"""
+
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True)
 
+
 class Item(Base):
+    """Base model for item"""
+
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    title= Column(String(50))
-    description= Column(String(100))
-    user_id=Column(Integer)
+    title = Column(String(50))
+    description = Column(String(100))
+    user_id = Column(Integer)
